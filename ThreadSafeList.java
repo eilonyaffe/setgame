0f17c215fa66ca284856f89c7e3e5b08a0a8c61f
Eilon Yaffe <93070667+eilonyaffe@users.noreply.github.com>
2024-02-19 22:04:52 +0200

    Update ThreadSafeList.java

--- ThreadSafeList.java
+++ ThreadSafeList.java
@@ -1,13 +1,12 @@
 package bguspl.set.ex;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import bguspl.set.Env;
 
 public class ThreadSafeList {
 
-    private List<Integer> list = new ArrayList<>();
+    protected ArrayList<Integer> list = new ArrayList<>();
     private final Env env;
     private int slot;
 
@@ -35,6 +34,18 @@ public class ThreadSafeList {
         return exists;
     }
 
+    public synchronized int[] removeAll() { //removes all players from this slot, removes their tokens from ui, and return their ids
+        if (list == null) return null;
+
+        int[] players = new int[this.list.size()];
+        for(int i=0;i<players.length;i++){
+            players[i] = this.list.get(i).intValue();
+            env.ui.removeToken(players[i], this.slot);
+        }
+        this.list.clear();
+        return players;
+    }
+
     public synchronized boolean contains(int value) {
         if (list == null) return false;
         return list.contains(value);
@@ -43,6 +54,14 @@ public class ThreadSafeList {
     public synchronized int size() {
         if (list == null) return -1;
         return list.size();
+    }
+
+    public synchronized int[] getPlayers() { //TODO
+        int[] players = new int[this.list.size()];
+        for(int i=0;i<players.length;i++){
+            players[i] = this.list.get(i).intValue();
+        }
+        return players;
     }
 
     //EYTODO delete later, used for testing
